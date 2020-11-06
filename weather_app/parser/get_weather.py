c61ad5e5aaf42fad089ab4e345c0d767e6824f02
Iglek Kuat <dizinnes228@gmail.com>
2020-11-07 01:12:15 +0600

    refactor: change variables name

--- weather_app/parser/get_weather.py
+++ weather_app/parser/get_weather.py
@@ -7,13 +7,13 @@ class GetWeather:
     def __init__(self, city_name: str) -> None:
         self.__API = OWM(env('OPENWEATHERAPI'))
         self.__MANAGER = self.__API.weather_manager()
-        self.__OBSERVATION = self.__MANAGER.weather_at_place(city_name)
+        self.__WEATHER = self.__MANAGER.weather_at_place(city_name)
 
-    def get_weather(self) -> dict[str: int or None]:
-        return self.__OBSERVATION.weather.temperature('celsius')
+    def get_temperature(self) -> dict[str: int or None]:
+        return self.__WEATHER.weather.temperature('celsius')
 
     def get_humidity(self) -> int:
-        return self.__OBSERVATION.weather.humidity
+        return self.__WEATHER.weather.humidity
 
     def get_wind(self) -> tuple[str, int]:
-        return next(iter(self.__OBSERVATION.weather.wind().items()))
+        return next(iter(self.__WEATHER.weather.wind().items()))
