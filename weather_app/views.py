150cf19e9ba89c74466dd914e34307cc0db80a0a
Iglek Kuat <dizinnes228@gmail.com>
2020-11-07 01:12:29 +0600

    feat: add test view home page

--- weather_app/views.py
+++ weather_app/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 
+from .parser import GetWeather
+
 
 def home(request):
-    return render(request, 'weather_app/index.html')
+    weather = GetWeather('kokshetau')
+    context = {
+        'temperature': weather.get_temperature(),
+        'humidity': weather.get_humidity(),
+        'wind': weather.get_wind(),
+    }
+    return render(request, 'weather_app/index.html', context)
