28bf313bc64672bceb49f1ed884f3b6b5edb8839
Iglek Kuat <dizinnes228@gmail.com>
2020-11-08 16:37:51 +0600

    test: add test code

--- weather_app/views.py
+++ weather_app/views.py
@@ -4,11 +4,20 @@ from .parser import GetWeather
 
 
 def home(request):
-    city_name = str(request.POST.get('city'))
-    weather = GetWeather(str(city_name))
-    context = {
-        'temperature': weather.get_temperature(),
-        'humidity': weather.get_humidity(),
-        'wind': weather.get_wind(),
-    }
+    city_name = str(request.POST.get('city_name'))
+
+    if city_name is None:
+        print('I am none')
+        ...
+    elif city_name is not None:
+        print('City name:', city_name)
+        weather = GetWeather(str(city_name))
+        context = {
+            'temperature': weather.get_temperature(),
+            'humidity': weather.get_humidity(),
+            'wind': weather.get_wind(),
+        }
+    else:
+        print('error')
+
     return render(request, 'weather_app/index.html', context)
