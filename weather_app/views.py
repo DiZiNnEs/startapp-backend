3840ff14486f0519c9b37cf7a415a7d457ae4535
Iglek Kuat <dizinnes228@gmail.com>
2020-11-10 21:25:22 +0600

    temp

--- weather_app/views.py
+++ weather_app/views.py
@@ -5,19 +5,16 @@ from .parser import GetWeather
 
 def home(request):
     city_name = str(request.POST.get('city_name'))
-
-    if city_name is None:
-        print('I am none')
-        ...
-    elif city_name is not None:
+    while city_name is 'None':
         print('City name:', city_name)
-        weather = GetWeather(str(city_name))
-        context = {
-            'temperature': weather.get_temperature(),
-            'humidity': weather.get_humidity(),
-            'wind': weather.get_wind(),
-        }
-    else:
-        print('error')
+        if city_name is not 'None':
+            break
+
+    weather = GetWeather(str(city_name))
+    context = {
+        'temperature': weather.get_temperature(),
+        'humidity': weather.get_humidity(),
+        'wind': weather.get_wind(),
+    }
 
     return render(request, 'weather_app/index.html', context)
