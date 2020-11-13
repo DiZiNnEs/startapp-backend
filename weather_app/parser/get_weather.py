e0eca13de6ec3fc1dc9ee2ee2d04607f2836a126
Iglek Kuat <dizinnes228@gmail.com>
2020-11-13 19:57:26 +0600

    refactor: small updates and add all images (test variant)

--- weather_app/parser/get_weather.py
+++ weather_app/parser/get_weather.py
@@ -9,16 +9,23 @@ class GetWeather:
         self.__CITY_NAME = city_name
         self.__API = OWM(env('OPENWEATHERAPI'))
         self.__MANAGER = self.__API.weather_manager()
+
+    def __get_weather(self):
+        # try:
+        #     self.__WEATHER = self.__MANAGER.weather_at_place(self.__CITY_NAME)
+        # except exceptions.NotFoundError:
+        #     self.__WEATHER = self.__MANAGER.weather_at_place('Астана')
         try:
-            self.__WEATHER = self.__MANAGER.weather_at_place(self.__CITY_NAME)
+            weather_result = self.__MANAGER.weather_at_place(self.__CITY_NAME)
         except exceptions.NotFoundError:
-            self.__WEATHER = self.__MANAGER.weather_at_place('Астана')
+            weather_result = self.__MANAGER.weather_at_place('Astana')
+        return weather_result
 
     def get_temperature(self) -> dict[str: int or None]:
-        return self.__WEATHER.weather.temperature('celsius')
+        return self.__get_weather().weather.temperature('celsius')
 
     def get_humidity(self) -> int:
-        return self.__WEATHER.weather.humidity
+        return self.__get_weather().weather.humidity
 
     def get_wind(self) -> tuple[str, int]:
-        return next(iter(self.__WEATHER.weather.wind().items()))
+        return next(iter(self.__get_weather().weather.wind().items()))
