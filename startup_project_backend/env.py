842ea2add2a078a031e987514eb066c55d57d8f2
Iglek Kuat <dizinnes228@gmail.com>
2020-11-13 20:45:55 +0600

    feat: update for production #1

--- startup_project_backend/env.py
+++ startup_project_backend/env.py
@@ -7,5 +7,24 @@ load_dotenv()
 def env(key: str) -> str:
     value = getenv(key)
     if value is None:
-        raise EnvironmentError('Key with this name was not found in the file .env')
+        raise EnvironmentError(f'no key {key} in .env file')
     return value
+
+
+def env_bool(key: str) -> bool:
+    value = env(key)
+    if value == 'True':
+        return True
+    if value == 'False':
+        return False
+    raise EnvironmentError(f'key {key} can only be True or False')
+
+
+def env_int(key: str) -> int:
+    value = env(key)
+    return int(value)
+
+
+def env_float(key: str) -> float:
+    value = env(key)
+    return float(value)
--- startup_project_backend/settings.py
+++ startup_project_backend/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+from . import env
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'u9j!dad@t+0zf+@j$mdp4-_)jzcz^99woez1swwb+hm(i6+c&a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.env_bool('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [env.env('HOST')]
 
 
 # Application definition
