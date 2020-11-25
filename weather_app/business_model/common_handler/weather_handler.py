4e954a9b17acf5d018a7729fafdb7b23ec15ec25
Iglek Kuat <dizinnes228@gmail.com>
2020-11-25 14:01:12 +0600

    refactor: update try... except... block

--- weather_app/business_model/common_handler/weather_handler.py
+++ weather_app/business_model/common_handler/weather_handler.py
@@ -1,9 +1,9 @@
 def common_handler_function(recommendation_dictionary: dict[int: str]) -> str:
-    for temperature in recommendation_dictionary:
-        try:
+    try:
+        for temperature in recommendation_dictionary:
             if temperature <= temperature:
                 return recommendation_dictionary[temperature]
             else:
                 continue
-        except Exception as ex:
-            print(ex)
+    except Exception as ex:
+        print(ex)
